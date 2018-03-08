28069b7ffb62d1a86a06e2d2d0ba2c77168afa33
Xiaoming Sun <rsun2014@gmail.com>
2018-03-07 23:43:12 -0800

    more for annotation autowired

--- spring-intro/src/main/java/com/xiaoming/spring/basic_annotation_injection/AppConfig.java
+++ spring-intro/src/main/java/com/xiaoming/spring/basic_annotation_injection/AppConfig.java
@@ -4,7 +4,8 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.xiaoming.spring.basic_annotation_injection")
+@ComponentScan({"com.xiaoming.spring.basic_annotation_injection.autowired_bean",
+        "com.xiaoming.spring.basic_annotation_injection.repository"})
 public class AppConfig {
 
 }
