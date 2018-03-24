364f098adf279c8b9aa83a31101ec64b6a0c8f43
Xiaoming Sun <rsun2014@gmail.com>
2018-03-23 21:08:56 -0700

    Hibernate server

---
+++ hibernate/db_schema.sql
@@ -0,0 +1,7 @@
+create table t_student (
+    id int(11) not null auto_increment,
+    name varchar(225),
+    age int(11),
+    score double,
+    primary key (id)
+)
---
+++ hibernate/src/main/java/pers/xiaoming/java_framework/entity/Server.java
@@ -0,0 +1,36 @@
+package pers.xiaoming.java_framework.entity;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Server implements AutoCloseable {
+    private static SessionFactory sessionFactory;
+
+    private Server() {
+
+    }
+
+    public static Session getSession() {
+        if (sessionFactory == null) {
+            synchronized (Server.class) {
+                sessionFactory = initSession();
+            }
+        }
+        return sessionFactory.getCurrentSession();
+    }
+
+    @SuppressWarnings("UnnecessaryLocalVariable")
+    private static SessionFactory initSession() {
+        Configuration config = new Configuration().configure();
+
+        SessionFactory sessionFactory = config.buildSessionFactory();
+
+        return sessionFactory;
+    }
+
+    @Override
+    public void close() throws Exception {
+        sessionFactory.close();
+    }
+}
