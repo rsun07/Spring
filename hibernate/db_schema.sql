1fbd0f152c655c1c24a146d205adfdf930381fa5
Xiaoming Sun <rsun2014@gmail.com>
2018-03-25 01:02:21 -0700

    Move hibernate out of this project

--- hibernate/db_schema.sql
+++
@@ -1,7 +0,0 @@
-create table t_student (
-    t_id int(11) not null auto_increment,
-    t_name varchar(225),
-    t_age int(11),
-    t_score double,
-    primary key (t_id)
-)
--- hibernate/src/main/java/pers/xiaoming/java_framework/command/GetStudent.java
+++
@@ -1,8 +0,0 @@
-package pers.xiaoming.java_framework.command;
-
-import org.hibernate.Session;
-import pers.xiaoming.java_framework.entity.Student;
-
-public interface GetStudent {
-    Student get(Session session, int id);
-}
