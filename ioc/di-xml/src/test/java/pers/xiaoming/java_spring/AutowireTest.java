47c6c67c02e8f1c8fdee512b79222b763120db03
Xiaoming Sun <rsun2014@gmail.com>
2018-04-25 10:57:22 -0700

    autowire

---
+++ ioc/di-xml/src/test/java/pers/xiaoming/java_spring/AutowireTest.java
@@ -0,0 +1,30 @@
+package pers.xiaoming.java_spring;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AutowireTest {
+    @Test
+    public void test() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("autowireContext.xml");
+        Student actual = (Student) ac.getBean("student");
+        Student expect = getExpectStudent();
+
+        Assert.assertEquals(actual, expect);
+    }
+
+    private Student getExpectStudent() {
+        Address address = new Address(8888, "CA DR", 12345);
+
+        School school = new School("MySchool");
+        school.setAddress(address);
+
+        Student student = new Student();
+        student.setName("John");
+        student.setAge(23);
+        student.setSchool(school);
+        return student;
+    }
+}
