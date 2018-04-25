dad91d836cb01b4118e1f0d4ffbafbe9b636e0ba
Xiaoming Sun <rsun2014@gmail.com>
2018-04-24 20:36:19 -0700

    Spring life cycle'

--- spring-basic/src/main/java/pers/xiaoming/java_spring/spring_basic/bean_life_cycle/MyBean.java
+++ spring-basic/src/main/java/pers/xiaoming/java_spring/spring_basic/bean_life_cycle/MyBean.java
@@ -4,12 +4,22 @@ package pers.xiaoming.java_spring.spring_basic.bean_life_cycle;
 public class MyBean {
     private String name;
 
+    {
+        System.out.println("Running Dynamic code block, my bean name is : " + this.name);
+    }
+
     MyBean() {
+        this.name = "Default_name";
+        System.out.println("Constructing...");
+    }
+
+    MyBean(String name) {
+        this.name = name;
         System.out.println("Constructing...");
     }
 
     void initMethod() {
-        System.out.println("Init...");
+        System.out.println("Init..., my bean name is : " + name);
     }
 
     // not the destroy from java Interface
--- spring-basic/src/test/java/pers/xiaoming/java_spring/spring_basic/bean_life_cycle/LifeCycleTest.java
+++ spring-basic/src/test/java/pers/xiaoming/java_spring/spring_basic/bean_life_cycle/LifeCycleTest.java
@@ -4,8 +4,36 @@ import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class LifeCycleTest {
+    /*
+        Running Dynamic code block, my bean name is : null
+        Constructing...
+        Setting name to <My Bean!>
+        My Bean post process before init method
+        Init..., my bean name is : My Bean!
+        My Bean post process after init method
+        Setting name to <Post Process Reset Name>
+     */
     @Test
-    public void test() {
+    public void testOnlyInitContainer() {
+        ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beanLifeCycle.xml");
+    }
+
+    /*
+        Running Dynamic code block, my bean name is : null
+        Constructing...
+        Setting name to <My Bean!>
+        My Bean post process before init method
+        Init..., my bean name is : My Bean!
+        My Bean post process after init method
+        Setting name to <Post Process Reset Name>
+        Apr 24, 2018 8:35:51 PM org.springframework.context.support.ClassPathXmlApplicationContext doClose
+        INFO: Closing org.springframework.context.support.ClassPathXmlApplicationContext@5cbc508c: startup date [Tue Apr 24 20:35:51 PDT 2018]; root of context hierarchy
+        MyBean{name='Post Process Reset Name'}
+
+        Destroying...
+     */
+    @Test
+    public void testGetBean() {
         ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("beanLifeCycle.xml");
         System.out.println(ctx.getBean("myBean").toString());
         ctx.destroy();
