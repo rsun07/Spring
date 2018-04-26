1c4ed5cfa517f0a63cf04f36d7c272465bf1a804
Xiaoming Sun <rsun2014@gmail.com>
2018-04-26 15:51:43 -0700

    test default advisor auto proxy creator

--- aop/aop-xml/src/test/java/pers/xiaoming/java_spring/advisor/AdvisorTest.java
+++ aop/aop-xml/src/test/java/pers/xiaoming/java_spring/advisor/AdvisorTest.java
@@ -13,34 +13,38 @@ public class AdvisorTest {
     @Test
     public void testBeforeAdvisor() {
         AOPTarget target = (AOPTarget) ac.getBean("beforeAdvisorProxy");
-        target.print();
-        System.out.println();
-        target.toLowercase("Abc");
+        testTarget(target);
     }
 
     @Test
     public void testAfterAdvisor() {
         AOPTarget target = (AOPTarget) ac.getBean("afterAdvisorProxy");
-        target.print();
-        System.out.println();
-        target.toUppercase("Abc");
-        System.out.println();
-        target.toLowercase("Abc");
+        testTarget(target);
     }
 
     @Test
     public void testMethodInterceptorAdvisor() {
         AOPTarget target = (AOPTarget) ac.getBean("methodInterceptorAdvisorProxy");
-        target.print();
-        System.out.println();
-        target.toUppercase("Abc");
-        System.out.println();
-        target.toLowercase("Abc");
+        testTarget(target);
     }
 
     @Test
     public void testRegexAdvisor() {
         AOPTarget target = (AOPTarget) ac.getBean("regexAdvisorProxy");
+        testTarget(target);
+    }
+
+    @Test
+    public void testAutoAdvisorProxyCreator() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("advisorAutoCreatorContext.xml");
+
+        // get by target bean id directly rather than proxy bean id
+        AOPTarget target = (AOPTarget) ac.getBean("target");
+        testTarget(target);
+
+    }
+
+    private void testTarget(AOPTarget target) {
         target.print();
         System.out.println();
         target.toUppercase("Abc");
