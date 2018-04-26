1b3174d22cd211b6bc31d8733fa4e11f68dab99a
Xiaoming Sun <rsun2014@gmail.com>
2018-04-25 22:04:37 -0700

    classes for before, after advice and target interface and impl

---
+++ aop/aop-xml/src/main/java/pers/xiaoming/java_spring/AOPTarget.java
@@ -0,0 +1,7 @@
+package pers.xiaoming.java_spring;
+
+public interface AOPTarget {
+    void print();
+
+    String toUppercase(String str);
+}
---
+++ aop/aop-xml/src/main/java/pers/xiaoming/java_spring/AOPTargetImpl.java
@@ -0,0 +1,13 @@
+package pers.xiaoming.java_spring;
+
+public class AOPTargetImpl implements AOPTarget {
+
+    public void print() {
+        System.out.println("Executing AOP target class print()");
+    }
+
+    public String toUppercase(String str) {
+        System.out.println("Executing AOP target class output()");
+        return str.trim().toUpperCase();
+    }
+}
