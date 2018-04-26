ccb2a62e26f450973c72661f3ae6608e4c501366
Xiaoming Sun <rsun2014@gmail.com>
2018-04-26 15:26:28 -0700

    Test advisor

---
+++ aop/aop-xml/src/test/java/pers/xiaoming/java_spring/advisor/AdvisorTest.java
@@ -0,0 +1,40 @@
+package pers.xiaoming.java_spring.advisor;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pers.xiaoming.java_spring.entity.AOPTarget;
+
+@Ignore("demo test")
+public class AdvisorTest {
+    private ApplicationContext ac = new ClassPathXmlApplicationContext("advisorContext.xml");
+
+    @Test
+    public void testBeforeAdvisor() {
+        AOPTarget target = (AOPTarget) ac.getBean("beforeAdvisorProxy");
+        target.print();
+        System.out.println();
+        target.toLowercase("Abc");
+    }
+
+    @Test
+    public void testAfterAdvisor() {
+        AOPTarget target = (AOPTarget) ac.getBean("afterAdvisorProxy");
+        target.print();
+        System.out.println();
+        target.toUppercase("Abc");
+        System.out.println();
+        target.toLowercase("Abc");
+    }
+
+    @Test
+    public void testMethodInterceptorAdvisor() {
+        AOPTarget target = (AOPTarget) ac.getBean("methodInterceptorAdvisorProxy");
+        target.print();
+        System.out.println();
+        target.toUppercase("Abc");
+        System.out.println();
+        target.toLowercase("Abc");
+    }
+}
