53fdd3ca247c4b8ec66b3afe3c9039335a2bd9d4
Xiaoming Sun <rsun2014@gmail.com>
2018-04-26 12:49:23 -0700

    improve dynamic proxy test with assert and proint class info and comments

--- aop/background-proxy/src/test/java/pers/xiaoming/java_spring/jdk_dynamic_proxy/DynamicProxyTest.java
+++ aop/background-proxy/src/test/java/pers/xiaoming/java_spring/jdk_dynamic_proxy/DynamicProxyTest.java
@@ -1,26 +1,49 @@
 package pers.xiaoming.java_spring.jdk_dynamic_proxy;
 
+import org.junit.Assert;
 import org.junit.Test;
 import pers.xiaoming.java_spring.Calculator;
 import pers.xiaoming.java_spring.CalculatorImpl;
 
+import java.util.Arrays;
+
 public class DynamicProxyTest {
     private Calculator target = new CalculatorImpl();
 
+    // JDK dynamic proxy is reflection on interface
+    /*
+        Class name is : pers.xiaoming.java_spring.CalculatorImpl
+        Super Class is : class java.lang.Object
+        Interfaces are : [interface pers.xiaoming.java_spring.Calculator]
+
+        Class name is : com.sun.proxy.$Proxy4
+        Super Class is : class java.lang.reflect.Proxy
+        Interfaces are : [interface pers.xiaoming.java_spring.Calculator]
+     */
     @Test
     public void test() {
         Calculator proxy = new CalculatorLoggingProxy(target).getLoggingProxy();
 
-        proxy.add(2, 8);
-        proxy.sub(8, 3);
-        proxy.mul(8,3);
-        proxy.div(8,3);
+        Assert.assertEquals(10, proxy.add(2,8));
+        Assert.assertEquals(5, proxy.sub(8,3));
+        Assert.assertEquals(24, proxy.mul(8,3));
+        Assert.assertEquals(2, proxy.div(8,3));
 
         // CalculatorImpl
-        System.out.println(target.getClass().getName());
+        printClassInfo(target.getClass());
 
         // com.sun.proxy.$Proxy4
-        System.out.println(proxy.getClass().getName());
+        printClassInfo(proxy.getClass());
+    }
+
+    private void printClassInfo(Class myClass) {
+        System.out.printf("\nClass name is : %s\n"
+                + "Super Class is : %s\n"
+                + "Interfaces are : %s\n",
+                myClass.getName(),
+                myClass.getSuperclass(),
+                Arrays.asList(myClass.getInterfaces()));
+
     }
 
     /*
